482df7d62a60c307395d2f9650f3e1b8ad511c44
yu-val-weiss <56967322+Yu-val-weiss@users.noreply.github.com>
2023-12-29 10:11:01 +0100

    Add graceful shutdown

--- webapi/webapi.go
+++ webapi/webapi.go
@@ -1,11 +1,15 @@
 package webapi
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"strings"
+	"time"
 	"yu-val-weiss/yap/app"
 	"yu-val-weiss/yap/nlp/format/conll"
 	"yu-val-weiss/yap/nlp/format/lattice"
@@ -188,6 +192,9 @@ listen to api requests
 }
 
 func StartAPIServer(cmd *commander.Command, args []string) error {
+	var wait time.Duration
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*5, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.Parse()
 	HebrewMorphAnalyazerInitialize(cmd, args)
 	MorphDisambiguatorInitialize(cmd, args)
 	DepParserInitialize(cmd, args)
@@ -201,6 +208,38 @@ func StartAPIServer(cmd *commander.Command, args []string) error {
 	router.HandleFunc("/yap/heb/pipeline", HebrewPipelineHandler)
 	router.HandleFunc("/yap/heb/joint", HebrewJointHandler)
 	router.HandleFunc("/yap/heb/healthcheck", HealthCheckHandler)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), router))
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%v", port),
+		Handler: router,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Block until we receive our signal.
+	<-c
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	srv.Shutdown(ctx)
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	log.Println("shutting down")
+	os.Exit(0)
+
+	// log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), router))
 	return nil
 }
